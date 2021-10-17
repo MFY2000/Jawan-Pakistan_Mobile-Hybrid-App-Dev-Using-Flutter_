65d7a906943abc759a0e32b41f70b0ea5156f2cc
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-18 01:47:34 +0500

    Revert "Quiz Complete 1:47pm all done 18 Oct"

--- Tasks/Task07 (ListBuilder Quiz)/e_commerce/lib/Widget/Body.dart
+++ Tasks/Task07 (ListBuilder Quiz)/e_commerce/lib/Widget/Body.dart
@@ -1,49 +1,38 @@
 
 
-// ignore_for_file: file_names, non_constant_identifier_names, prefer_const_constructors, no_logic_in_create_state
+// ignore_for_file: file_names, non_constant_identifier_names, prefer_const_constructors
 
 import 'package:e_commerce/Widget/Compents/Heading.dart';
 import 'package:e_commerce/Widget/Compents/SingleItem.dart';
 import 'package:flutter/material.dart';
 
-class Body extends StatefulWidget {
-  final List<Map<String, dynamic>> productMap;
 
-  Body({Key? key,required this.productMap}) : super(key: key);
+class Body extends StatelessWidget {
+  // final List<Map<String, dynamic>> items;
+  List<String> itemsmy;
+  Body({ Key? key,required this.itemsmy }) : super(key: key);
 
-  @override
-  _BodyState createState() => _BodyState(productMap);
-}
-
-class _BodyState extends State<Body> {
-  final List<Map<String, dynamic>> productMap;
-
-  _BodyState(this.productMap);
-
-  Widget ProductDisplay(List<Map<String, dynamic>> productMap) {
-    return ListView.builder(
-        itemCount: productMap.length,
-        itemBuilder: (context, index) {
-          return SingleItem(rowDetails: productMap[index]);
-        });
-  }
-
+  
 
   @override
   Widget build(BuildContext context) {
 
-    return Column(
-      children: [
-        // Heading
-          Heading(value: productMap.length),
-        
-        Expanded(
-          child: SizedBox(
-            width: 340,
-            child: ProductDisplay(productMap)
+    return Container(
+      child: Column(
+        children: [
+          // Heading
+            Heading(value: itemsmy.length),
+
+            ListView.builder(
+              itemCount: itemsmy.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                title: Text(itemsmy[index]),
+            );
+            },
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
--- Tasks/Task07 (ListBuilder Quiz)/e_commerce/lib/Widget/Compents/SingleItem.dart
+++ Tasks/Task07 (ListBuilder Quiz)/e_commerce/lib/Widget/Compents/SingleItem.dart
@@ -1,5 +1,5 @@
 
-// ignore_for_file: file_names, non_constant_identifier_names, prefer_const_constructors, curly_braces_in_flow_control_structures
+// ignore_for_file: file_names, non_constant_identifier_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -12,81 +12,20 @@ class SingleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 10),
-      child: Stack(
-        clipBehavior: Clip.none, children: [
-          Container(
-            width: 350,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.only(top: 10),
-            height: 155,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.2),
-                  spreadRadius: 3,
-                  blurRadius: 7,
-                  offset: Offset(0, 7), // changes position of shadow
-                ),
-              ],
-            ),
-
-          
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(rowDetails["Image"],width: 100),
-
-                    SizedBox(width: 20),
-
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(rowDetails["Name"], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13, textBaseline:TextBaseline.ideographic)),
-                        
-                         SizedBox(height: 18,),
-
-                        Text(rowDetails["Category"],style: TextStyle(fontSize: 10)),
-
-                        Row(children: RatingStars(rowDetails["Rating"])),
-
-                        SizedBox(height: 4),
-                        
-                         Text('\$ ${rowDetails["Price"]}',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
-                        )
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-
-          Positioned(
-            right: 0,
-            bottom: -19,
-            child: CircleAvatar(
-                radius: 23,
-                backgroundColor: Colors.amber,
-                child: Icon( (Icons.shopping_bag), size: 24, color: Colors.black,)
-            ),
-          )
-        ],
-      ),
+        margin: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 50),
+        height: 100,
+        width: 200,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        boxShadow: [BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 5,
+          blurRadius: 7,
+          offset: Offset(0, 3), // changes position of shadow
+        ),
+        ]
+    ),
+      child: Text("13"),
     );
-  }
-
-
-  List<Widget> RatingStars(StarCount) {
-    List<Widget> Rating =[];
-
-    for (var i = 0; i < StarCount; i++) 
-      Rating.add(Icon( Icons.star,size: 18,));
-    
-    return Rating;
   }
 }
