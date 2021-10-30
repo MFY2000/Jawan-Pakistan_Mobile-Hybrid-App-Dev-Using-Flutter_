abe1659b5cd8291706c22fd30703ff6f5646bf15
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-30 20:01:33 +0500

    MFY auto commit at 30/10/2021 20:01:05

---
+++ Tasks/Task08 (Pages)/ecommerce_/lib/Screen/FoodAppScreen1.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class FoodAppScreen1 extends StatelessWidget {
+  const FoodAppScreen1({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
