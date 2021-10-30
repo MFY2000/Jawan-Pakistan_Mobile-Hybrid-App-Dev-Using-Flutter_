b6a9dad51bc086e8802f92530ee815a4f456a78f
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-30 20:01:34 +0500

    MFY auto commit at 30/10/2021 20:01:05

---
+++ Tasks/Task08 (Pages)/ecommerce_/lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:ecommerce_/Screen/BookAppScreen1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BookAppScreen1(),
+    );
+  }
+}
