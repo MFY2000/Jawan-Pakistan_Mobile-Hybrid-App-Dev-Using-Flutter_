0b7acdab6b5d808beb03bba689980a37838043a0
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-02 21:50:35 +0500

    MFY auto commit at 02/11/2021 21:50:23

--- Tasks/Task08 (Pages)/ecommerce_/lib/main.dart
+++ Tasks/Task08 (Pages)/ecommerce_/lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:ecommerce_/Custom/Components/Button.dart';
 import 'package:ecommerce_/Pages/FoodApp.dart';
+import 'package:ecommerce_/Screen/BookAppScreen1.dart';
+import 'package:ecommerce_/Screen/FoodAppScreen1.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,18 +16,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FoodApp(),
+      home: ChooseApp(),
     );
   }
 }
 
 class ChooseApp extends StatelessWidget {
-  const ChooseApp({ Key? key }) : super(key: key);
+  const ChooseApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FoodAppScreen1()),
+                );
+              },
+              child: Text("Food App"),
+              style: ButtonStyle(
+
+              )),
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BookAppScreen1()),
+              );
+            },
+            child: Text("Book App"),
+          )
+        ],
+      ),
     );
   }
 }
