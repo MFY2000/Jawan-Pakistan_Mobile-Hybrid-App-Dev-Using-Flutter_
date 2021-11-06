4746dac474082d2fea4d9414cedb66e2b8f7c567
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-06 18:29:18 +0500

    MFY auto commit at 06/11/2021 18:29:14

--- Tasks/Task09 (Todo App)/todo_app_1/lib/Pages/todoList.dart
+++ Tasks/Task09 (Todo App)/todo_app_1/lib/Pages/todoList.dart
@@ -1,23 +1,24 @@
 // ignore_for_file: file_names
 
-
 import 'package:flutter/material.dart';
 import 'package:todo_app_1/Model/todo.dart';
 
 class todoList_ extends StatelessWidget {
   List<todoModel> list;
 
-  todoList_({ Key? key, required this.list, }) : super(key: key);
+  todoList_({
+    Key? key,
+    required this.list,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: list.length ,itemBuilder: (context, index){
-      shrinkWrap: false;
-      physic: NeverScrollableScrollPhysics();
-      return ListTile(
-        title: Text("${list[index].data}"),
-        
-      );
-    });
+    return ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("${list[index].data}"),
+          );
+        });
   }
 }
