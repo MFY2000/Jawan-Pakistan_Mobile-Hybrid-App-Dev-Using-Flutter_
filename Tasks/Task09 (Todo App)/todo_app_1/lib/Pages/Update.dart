e957c42abaaf8229658483c4fd9d745351ab9058
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-07 10:01:24 +0500

    MFY auto commit at 07/11/2021 10:00:57

---
+++ Tasks/Task09 (Todo App)/todo_app_1/lib/Pages/Update.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+
+class updatetask extends StatefulWidget {
+  const updatetask({ Key? key }) : super(key: key);
+
+  @override
+  _updatetaskState createState() => _updatetaskState();
+}
+
+class _updatetaskState extends State<updatetask> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
