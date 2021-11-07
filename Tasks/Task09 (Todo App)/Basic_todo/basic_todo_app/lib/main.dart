c49463fe1bfafcd20e9bf3f92203a13abb4f57e3
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-07 10:01:13 +0500

    MFY auto commit at 07/11/2021 10:00:57

---
+++ Tasks/Task09 (Todo App)/Basic_todo/basic_todo_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:basic_todo_app/todo_list_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      home: TodoListScreen(),
+    );
+  }
+}
