99fa2f4ed99fa2a4e39a225b8abc584f2b0fae34
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-09 17:34:55 +0500

    MFY auto commit at 09/11/2021 17:34:49

--- Tasks/Task09 (Todo App)/todo_app_1/lib/Pages/AddTask.dart
+++ Tasks/Task09 (Todo App)/todo_app_1/lib/Pages/AddTask.dart
@@ -2,9 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:date_time_picker/date_time_picker.dart';
+import 'package:todo_app_1/Model/todo.dart';
 
 class addTask extends StatefulWidget {
-  const addTask({Key? key}) : super(key: key);
+  final int index;
+  const addTask({Key? key, required this.index}) : super(key: key);
 
   @override
   _addTaskState createState() => _addTaskState();
@@ -13,12 +15,20 @@ class addTask extends StatefulWidget {
 class _addTaskState extends State<addTask> {
   late double height;
   late double width_ = 00.00;
+  late todoModel todo;
+  TextEditingController taskInput = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    if(lstTodo.length == widget.index){
+      todo = todoModel(value: "", status: false, date: "");
+    }else{
+      todo = lstTodo[widget.index];
+    }
+    taskInput.text = todo.value;
     height = MediaQuery.of(context).size.height;
     width_ = MediaQuery.of(context).size.width;
-    String? _selectedDate;
+
 
     return Scaffold(
       appBar: AppBar(),
@@ -29,11 +39,13 @@ class _addTaskState extends State<addTask> {
         child: Column(
           children: [
             Container(
+              margin: EdgeInsets.all(width_ * .1),
               child: Row(
                 children: [
                   SizedBox(
-                    width: width_ * .855,
+                    width: width_ * .7,
                     child: TextField(
+                      controller: taskInput,
                       style: TextStyle(color: Colors. white),
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
@@ -42,10 +54,10 @@ class _addTaskState extends State<addTask> {
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.all(8),
+                    padding: EdgeInsets.only(left: 10),
                     child: Icon(
                       Icons.keyboard_voice,
-                      size: 16,
+                      size: 20,
                       color: Colors.white,
                     ),
                   ),
@@ -53,82 +65,99 @@ class _addTaskState extends State<addTask> {
               ),
             ),
             SizedBox(height: height * 0.01),
-            DateTimePicker(
-              initialValue: '', // initialValue
-              type: DateTimePickerType.date,
-              dateLabelText: 'Select Date',
-              firstDate: DateTime(1995),
-              lastDate: DateTime.now().add(Duration(
-                  days: 365)), // This will add one year from current date
-              validator: (value) {
-                return null;
-              },
-              onChanged: (value) {
-                if (value.isNotEmpty) {
-                  setState(() {
-                    _selectedDate = value;
-                  });
-                }
-              },
-              // We can also use onSaved
-              onSaved: (value) {
-                if (value!.isNotEmpty) {
-                  setState(() {
-                    _selectedDate = value;
-                  });
-                }
-              },
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Your Selected Date: $_selectedDate',
-              style: TextStyle(fontSize: 16),
-            ),
+            // DateTimePicker(
+            //   initialValue: '', // initialValue
+            //   type: DateTimePickerType.date,
+            //   dateLabelText: 'Select Date',
+            //   firstDate: DateTime(1995),
+            //   lastDate: DateTime.now().add(Duration(
+            //       days: 365)), // This will add one year from current date
+            //   validator: (value) {
+            //     return null;
+            //   },
+            //   onChanged: (value) {
+            //     if (value.isNotEmpty) {
+            //       setState(() {
+            //         todo.date = value;
+            //       });
+            //     }
+            //   },
+            //   // We can also use onSaved
+            //   onSaved: (value) {
+            //     if (value!.isNotEmpty) {
+            //       setState(() {
+            //         todo.date = value;
+            //       });
+            //     }
+            //   },
+            // ),
+            // SizedBox(height: 16),
+            // Text(
+            //   'Your Selected Date: ${todo.date}',
+            //   style: TextStyle(fontSize: 16),
+            // ),
          
-            SizedBox(height: height * 0.01),
-            DateTimePicker(
-              initialValue: '', // initialValue
-              type: DateTimePickerType.time,
-              timeLabelText: 'Select Time',
-              initialTime: TimeOfDay(hour: 7,minute: 11),
-              lastDate: DateTime.now().add(Duration(hours: 12)),
+            // SizedBox(height: height * 0.01),
+            // DateTimePicker(
+            //   initialValue: '', // initialValue
+            //   type: DateTimePickerType.time,
+            //   timeLabelText: 'Select Time',
+            //   initialTime: TimeOfDay(hour: 7,minute: 11),
+            //   lastDate: DateTime.now().add(Duration(hours: 12)),
               
-              validator: (value) {
-                return null;
-              },
-              onChanged: (value) {
-                if (value.isNotEmpty) {
-                  setState(() {
-                    _selectedDate = value;
-                  });
-                }
-              },
-              // We can also use onSaved
-              onSaved: (value) {
-                if (value!.isNotEmpty) {
-                  setState(() {
-                    _selectedDate = value;
-                  });
-                }
-              },
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Your Selected Date: $_selectedDate',
-              style: TextStyle(fontSize: 16),
-            ),
+            //   validator: (value) {
+            //     return null;
+            //   },
+            //   onChanged: (value) {
+            //     if (value.isNotEmpty) {
+            //       setState(() {
+            //         todo.date = value;
+            //       });
+            //     }
+            //   },
+            //   // We can also use onSaved
+            //   onSaved: (value) {
+            //     if (value!.isNotEmpty) {
+            //       setState(() {
+            //         todo.date = value;
+            //       });
+            //     }
+            //   },
+            // ),
+            // SizedBox(height: 16),
+            // Text(
+            //   'Your Selected Date: ${todo.date}',
+            //   style: TextStyle(fontSize: 16),
+            // ),
           
           
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: saveTaskInput,
         foregroundColor: const Color(0XFF036FB2),
         backgroundColor: Colors.white,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.check),
       ),
     );
   }
+
+  saveTaskInput(){
+    if(taskInput.value.text.isNotEmpty){
+      if(lstTodo.length == widget.index){
+        setState(() {
+          lstTodo.add(todo);
+        });
+      }
+      setState(() { todo.value = taskInput.value.text; });
+      Navigator.pop(context);
+    }
+  }
+
+  GoBack(){
+    Navigator.pop(context);
+  }
+  
 }
