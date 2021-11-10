d1977fe29f2c0e0b10ec706e2c73ab7bbeda755b
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-10 16:55:39 +0500

    MFY auto commit at 10/11/2021 16:55:33

--- Tasks/Task09 (Todo App)/todo_app_1/lib/Model/todo.dart
+++ Tasks/Task09 (Todo App)/todo_app_1/lib/Model/todo.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class todoModel {
   String value;
   String date;
@@ -16,3 +19,18 @@ List<todoModel> lstTodo = [
   todoModel(value: "To go", status: false, date: ""),
   todoModel(value: "To go", status: false, date: ""),
 ];
+
+class opertion {
+  String name;
+  IconData iconData;
+  Color color;
+  
+  opertion({required this.color, required this.name, required this.iconData});
+}
+
+List<opertion> lstOperation = [
+  opertion(color: const Color(0XFF036FB2), name: "Add", iconData: Icons.add),
+  opertion(color: const Color(0XFF036FB2), name: "Update", iconData: Icons.update),
+  opertion(color: Colors.red, name: "Delete", iconData: Icons.delete),
+];
+
