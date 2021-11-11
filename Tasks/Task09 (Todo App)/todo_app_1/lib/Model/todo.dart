63e7249195417f3f777ecb9c688a7bc58bcf1c89
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-11 14:22:30 +0500

    MFY auto commit at 11/11/2021 14:22:12

--- Tasks/Task09 (Todo App)/todo_app_1/lib/Model/todo.dart
+++ Tasks/Task09 (Todo App)/todo_app_1/lib/Model/todo.dart
@@ -9,6 +9,10 @@ class todoModel {
 
 
   todoModel({required this.value, required this.status, this.check = false,required this.date});
+
+  void toRefresh(){
+    check = false;
+  }
 }
 
 List<todoModel> lstTodo = [
