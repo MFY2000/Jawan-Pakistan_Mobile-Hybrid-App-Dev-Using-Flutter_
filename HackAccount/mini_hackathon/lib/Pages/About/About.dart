a8ecda4b0d448b13084dfe8fbfddb6cad02acbd2
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-20 22:57:28 +0500

    MFY auto commit at 20/11/2021 22:40:31

--- HackAccount/mini_hackathon/lib/Pages/About/About.dart
+++ HackAccount/mini_hackathon/lib/Pages/About/About.dart
@@ -2,27 +2,25 @@
 
 import 'package:flutter/material.dart';
 
-
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SingleChildScrollView(
         child: Column(
           children: [
             Container(
               color: Colors.blue[800],
-              width: 400,
+              width: MediaQuery.of(context).size.width,
               height: 300,
               child: Container(
-                padding: EdgeInsets.all(50),
+                padding: const EdgeInsets.all(50),
                 child: CircleAvatar(
                   radius: 50,
                   backgroundColor: Colors.blue[900],
-                  child: Icon(
+                  child: const Icon(
                     Icons.mail_outline,
                     color: Colors.white,
                     size: 120,
@@ -32,42 +30,36 @@ class AboutPage extends StatelessWidget {
             ),
             Column(
               children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Icon(Icons.location_on, color: Colors.deepOrange),
-                SizedBox(
-                  height: 20,
-                ),
-                Text('1399 city hotel'),
-                SizedBox(
-                  height: 20,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text("Open Map"),
-                ),
-                Icon(Icons.mobile_friendly_sharp,
-                    color:Colors.deepOrange),
-                SizedBox(
-                  height: 20,
-                ),
-                Text('0301298880'),
-                SizedBox(
-                  height: 20,
-                ),
-                Icon(Icons.history, color:Colors.deepOrange),
-                SizedBox(
-                  height: 20,
-                ),
-                Text('Monday - Friday'),
-                SizedBox(
-                  height: 20,
-                ),
-                Text('09:00-17:00'),
-                SizedBox(
-                  height: 20,
-                ),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Icon(Icons.location_on,
+                        color: Colors.deepOrange)),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Text('1399 city hotel')),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text("Open Map"),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Icon(Icons.mobile_friendly_sharp,
+                        color: Colors.deepOrange)),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Text('0301298880')),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Icon(Icons.history, color: Colors.deepOrange)),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Text('Monday - Friday')),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: const Text('09:00-17:00')),
+                
               ],
             ),
           ],
