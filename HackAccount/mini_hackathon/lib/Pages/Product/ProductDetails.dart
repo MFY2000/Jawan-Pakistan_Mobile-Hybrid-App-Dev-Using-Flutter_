97855ef1785b6fc2fc78f9837d2fc08559fc2a4c
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-21 00:02:08 +0500

    MFY auto commit at 21/11/2021 00:00:04

--- HackAccount/mini_hackathon/lib/Pages/Product/ProductDetails.dart
+++ HackAccount/mini_hackathon/lib/Pages/Product/ProductDetails.dart
@@ -1,19 +1,66 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:mini_hackathon/Pages/Home/HomeScreen.dart';
+import 'package:mini_hackathon/model/Product.dart';
 
 class ProductDetails extends StatefulWidget {
-  const ProductDetails({ Key? key }) : super(key: key);
+  final Product item;
+  const ProductDetails({Key? key, required this.item}) : super(key: key);
 
   @override
   _ProductDetailsState createState() => _ProductDetailsState();
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
+  late Product item;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    item = widget.item;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        foregroundColor: Colors.deepOrange,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            onPressed: () => {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()))
+                },
+            icon: const Icon(
+              Icons.keyboard_arrow_left,
+              size: 30,
+            ),
+            color: Colors.deepOrange),
+      ),
+      body: Card(
+        child: Expanded(
+            child: Column(
+          children: [
+            Image.asset(
+              item.productImage,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * .45,
+            ),
+            Text(
+              item.productName,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
