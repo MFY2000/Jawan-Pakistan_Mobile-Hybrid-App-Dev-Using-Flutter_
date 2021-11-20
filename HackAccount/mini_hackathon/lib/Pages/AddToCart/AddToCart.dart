9563488e67ab37191e9a79fc529f32e651139a5f
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-20 22:57:29 +0500

    MFY auto commit at 20/11/2021 22:40:31

--- HackAccount/mini_hackathon/lib/Pages/AddToCart/AddToCart.dart
+++ HackAccount/mini_hackathon/lib/Pages/AddToCart/AddToCart.dart
@@ -2,30 +2,59 @@
 
 import 'package:flutter/material.dart';
 import 'package:mini_hackathon/Pages/Home/Components/Product/ProductCard.dart';
+import 'package:mini_hackathon/Pages/Home/HomeScreen.dart';
 import 'package:mini_hackathon/model/Product.dart';
 
-class AddToCart extends StatelessWidget {
-  const AddToCart({ Key? key }) : super(key: key);
+class AddToCart extends StatefulWidget {
+  const AddToCart({Key? key}) : super(key: key);
 
+  @override
+  State<AddToCart> createState() => _AddToCartState();
+}
+
+class _AddToCartState extends State<AddToCart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        foregroundColor: Colors.deepOrange,
+        backgroundColor: Colors.white,
+        leading: IconButton(onPressed: ()=>{
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const HomeScreen()))
+            }, icon: const Icon(Icons.keyboard_arrow_left, size: 20,), color: Colors.deepOrange),
+        title: Row(
+          children: const [
+            Text(
+              "MyCart",style: TextStyle(color: Colors.black),
+            ),
+            Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.deepOrange,
+            ),
+          ],
+        ),
+      ),
       body: ListView.builder(
           shrinkWrap: true,
-          padding: EdgeInsets.only(top: 20),
-          
+          padding: const EdgeInsets.only(top: 20),
           itemCount: LstAddToCart.length,
           itemBuilder: (context, index) {
-            return ProductCard(item: LstProduct[LstAddToCart[index]], index: index,addToCart: onAddToCart, typeOfCard: 0,);
+            return ProductCard(
+              item: LstProduct[LstAddToCart[index]],
+              index: index,
+              addToCart: onAddToCart,
+              typeOfCard: 0,
+            );
           }),
     );
   }
 
   onAddToCart(int index) {
-    if (!LstAddToCart.contains(index)) {
-      
+    if (LstAddToCart.contains(index)) {
+      setState(() {
+        LstAddToCart.removeAt(index);
+      });
     }
   }
 }
-
