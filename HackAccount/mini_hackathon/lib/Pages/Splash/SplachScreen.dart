0eb499d2f73ea4e891b7f4967a27646dadf9ab5a
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-20 15:41:15 +0500

    MFY auto commit at 20/11/2021 15:40:54

--- HackAccount/mini_hackathon/lib/Pages/Splash/SplachScreen.dart
+++ HackAccount/mini_hackathon/lib/Pages/Splash/SplachScreen.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: file_names
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mini_hackathon/Pages/Login/LoginScreen.dart';
 
 class SplachScreen extends StatefulWidget {
   const SplachScreen({Key? key}) : super(key: key);
@@ -11,21 +14,40 @@ class SplachScreen extends StatefulWidget {
 
 class SplachScreenState extends State<SplachScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginScreen())));
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepOrange,
-      child: Expanded(
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.deepOrange,
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text("Loading", style: TextStyle(
-              fontSize: 24,
-              color: Colors.black54,
-            ),),
-
-            CircularProgressIndicator(),
-
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: const Text(
+                "Loading",
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const CircularProgressIndicator(),
           ],
         ),
       ),
