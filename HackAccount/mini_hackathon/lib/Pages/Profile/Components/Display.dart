ba7e72d03315336b8fd8812abe3b28b417495ebf
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-20 22:57:33 +0500

    MFY auto commit at 20/11/2021 22:40:31

--- HackAccount/mini_hackathon/lib/Pages/Profile/Components/Display.dart
+++ HackAccount/mini_hackathon/lib/Pages/Profile/Components/Display.dart
@@ -5,16 +5,29 @@ import 'package:flutter/material.dart';
 class Display extends StatelessWidget {
   String heading;
   String subheading;
-  Display({ Key? key, required this.heading, required this.subheading}) : super(key: key);
+  Display({Key? key, required this.heading, required this.subheading})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 10),
+      alignment: Alignment.topLeft,
+      padding: const EdgeInsets.all(20),
       child: Column(
         children: [
-          Text(heading, style: TextStyle(color: Colors.deepOrange),),
-          Text(subheading, style: TextStyle(color: Colors.black38),),
+          Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                heading,
+                style: const TextStyle(color: Colors.deepOrange),
+                textAlign: TextAlign.left,
+              )),
+          Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                subheading,
+                style: const TextStyle(color: Colors.black38),
+              )),
         ],
       ),
     );
