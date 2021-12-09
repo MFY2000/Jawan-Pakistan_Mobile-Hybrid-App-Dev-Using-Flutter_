176542aa4edf41b4d6e746975c0d3995cd86cadd
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-09 06:49:46 +0500

    MFY auto commit at 09/12/2021 06:49:06

--- Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Splash/Splash.dart
+++ Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Splash/Splash.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 
+import 'package:fb_login_app/Pages/Login/Intro/Introduction.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -19,19 +20,20 @@ class _SplashScreenState extends State<SplashScreen>
   late Animation<double> animation;
 
   startTime() async {
-    var _duration = Duration(seconds: 3);
+    var _duration = const Duration(seconds: 6);
     return Timer(_duration, navigationPage);
   }
 
   void navigationPage() {
-    Navigator.of(context).pushReplacementNamed();
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (BuildContext buildContext) => const IntroSrceen()));
   }
 
   @override
   void initState() {
     super.initState();
     animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
     animation =
         CurvedAnimation(parent: animationController, curve: Curves.easeOut);
 
@@ -47,33 +49,36 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.only(bottom: 30.0),
-                  child: Image.asset(
-                    'assets/images/powered_by.png',
-                    height: 25.0,
-                    fit: BoxFit.scaleDown,
-                  ))
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/images/logo.png',
-                width: animation.value * 250,
-                height: animation.value * 250,
-              ),
-            ],
-          ),
-        ],
+      body: Container(
+        color: const Color(0XFF303030),
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: Image.asset(
+                      'assets/images/from.png',
+                      height: 25.0,
+                      fit: BoxFit.scaleDown,
+                    ))
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/logo/small_kolachi.png',
+                  width: animation.value * 250,
+                  height: animation.value * 250,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
