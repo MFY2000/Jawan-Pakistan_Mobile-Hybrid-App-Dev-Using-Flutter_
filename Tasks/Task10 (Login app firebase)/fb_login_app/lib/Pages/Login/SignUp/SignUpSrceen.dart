b95671cbdf1811bd1998c7816f4835affefdd67f
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-10 22:08:09 +0500

    MFY auto commit at 10/12/2021 22:08:05

---
+++ Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Login/SignUp/SignUpSrceen.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class SignInSrceen extends StatefulWidget {
+  const SignInSrceen({ Key? key }) : super(key: key);
+
+  @override
+  _SignInSrceenState createState() => _SignInSrceenState();
+}
+
+class _SignInSrceenState extends State<SignInSrceen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text("Signup"),
+          TextField(),
+          TextField(),
+          
+        ],
+      ),
+    );
+  }
+}
