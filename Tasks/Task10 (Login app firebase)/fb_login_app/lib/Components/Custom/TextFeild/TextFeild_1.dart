1d5b8824bf31437a91aaf3ca286f8118f2b65e6d
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-16 18:57:18 +0500

    MFY auto commit at 16/12/2021 18:55:35

--- Tasks/Task10 (Login app firebase)/fb_login_app/lib/Components/Custom/TextFeild/TextFeild_1.dart
+++ Tasks/Task10 (Login app firebase)/fb_login_app/lib/Components/Custom/TextFeild/TextFeild_1.dart
@@ -6,16 +6,15 @@ import 'package:flutter/material.dart';
 
 class TextFeild1 extends StatefulWidget {
   final TextFeildModel reference;
-  const TextFeild1({required this.reference ,Key? key}) : super(key: key);
+  const TextFeild1({required this.reference, Key? key}) : super(key: key);
 
   @override
   _TextFeild1State createState() => _TextFeild1State();
 }
 
 class _TextFeild1State extends State<TextFeild1> {
-  
   late TextFeildModel ref;
-  
+
   @override
   void initState() {
     // TODO: implement initState
@@ -26,7 +25,7 @@ class _TextFeild1State extends State<TextFeild1> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        alignment:Alignment.centerLeft,
+        alignment: Alignment.centerLeft,
         padding: EdgeInsets.symmetric(
           vertical: getSize(false, .0125),
         ),
@@ -34,16 +33,16 @@ class _TextFeild1State extends State<TextFeild1> {
           onChanged: ref.onChange,
           controller: ref.control,
           decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 0),
+            label: Text(ref.label),
             hintText: ref.label,
-            
+            alignLabelWithHint: true,
+            errorText: widget.reference.isError ? ref.errorMessage : null,
             enabledBorder: const UnderlineInputBorder(
-              borderSide: BorderSide(color: Color(0XFFE0E0E0), width: 2.3),
-            ),
+                borderSide: BorderSide(color: Color(0XFFE0E0E0), width: 2.3)),
             focusedBorder: const UnderlineInputBorder(
-              borderSide: BorderSide(color: Color(0xffbdbdbd), width: 2.3),
-            ),
-          
-          ),  
+                borderSide: BorderSide(color: Color(0xffbdbdbd), width: 2.3)),
+          ),
           textInputAction: TextInputAction.next,
         ));
   }
