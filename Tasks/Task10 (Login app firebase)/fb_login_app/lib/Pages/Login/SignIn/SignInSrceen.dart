750f7f8dc5cd55dfd4a00d8fe127cbe4575653f8
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-16 18:57:20 +0500

    MFY auto commit at 16/12/2021 18:55:35

--- Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Login/SignIn/SignInSrceen.dart
+++ Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Login/SignIn/SignInSrceen.dart
@@ -6,6 +6,7 @@ import 'package:fb_login_app/Components/Custom/TextFeild/TextFeild_1.dart';
 import 'package:fb_login_app/Config/size_config.dart';
 import 'package:fb_login_app/Model/TextFeildModel.dart';
 import 'package:fb_login_app/Pages/Home/HomeScreen.dart';
+import 'package:fb_login_app/Pages/Login/SignIn/ForgetPassword.dart';
 import 'package:fb_login_app/Pages/Login/SocialMeida/SocialMedia.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +26,7 @@ class _SignInSrceenState extends State<SignInSrceen> {
     // TODO: implement initState
     super.initState();
     controller = [
-      TextFeildModel(label: "Name"),
+      TextFeildModel(label: "Email"),
       TextFeildModel(label: "Password")
     ];
   }
@@ -51,30 +52,60 @@ class _SignInSrceenState extends State<SignInSrceen> {
               TextFeild1(reference: controller[0]),
               PasswordFeild(reference: controller[1]),
               Container(
+                  margin: EdgeInsets.only(top: getSize(false, .0125)),
                   alignment: Alignment.centerRight,
-                  child: GestureDetector(child: Text("Forget Password ?"))),
+                  child: GestureDetector(
+                      onTap: () {goToOtherRouter(const ForgetPassword());},
+                      child: Text(
+                        "Forget Password ?",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ))),
               Padding(
-                padding: EdgeInsets.symmetric(vertical: getSize(false, .0125)),
+                padding: EdgeInsets.symmetric(
+                  vertical: getSize(false, .0125),
+                ),
                 child: ButtonColored(function: onSigupClick, text: "Login"),
               ),
-              SocialMedia(),
+              const SocialMedia(
+                isSignIN: false,
+              ),
             ],
           ),
         ),
       ),
     );
   }
+  goToOtherRouter(Widget route) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (BuildContext context) => route));
+  }
 
   onSigupClick() {
-    if (controller[0].isFill) {
-    } else if (controller[1].isFill) {
-    } else if (controller[2].isFill) {
-    } else {
-      onSigup();
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (BuildContext context) => const HomeScreen()));
+    bool isError = false;
+
+    if (!controller[0].isFill) {
+      setState(() {
+        controller[0].isError = true;
+        controller[0].errorMessage = "User Id is empty";
+        isError = true;
+      });
+    }
+
+    if (!controller[1].isFill) {
+      setState(() {
+        controller[1].isError = true;
+        controller[1].errorMessage = "Passwords is empty";
+        isError = true;
+      });
+    }
+
+    if (!isError) {
+      print("hello");
+      // onSigup();
+      // Navigator.pushReplacement(
+      //     context,
+      //     MaterialPageRoute(
+      //         builder: (BuildContext context) => const HomeScreen()));
     }
   }
 
@@ -82,15 +113,14 @@ class _SignInSrceenState extends State<SignInSrceen> {
     try {
       UserCredential userCredential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(
-              email: controller[1].value, password: controller[2].value);
+              email: "barry.allen@example.com",
+              password: "SuperSecretPassword!");
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
       }
-    } catch (e) {
-      print(e);
     }
   }
 }
