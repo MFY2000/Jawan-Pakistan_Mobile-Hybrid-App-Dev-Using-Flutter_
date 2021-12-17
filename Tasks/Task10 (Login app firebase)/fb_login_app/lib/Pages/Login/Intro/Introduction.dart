9129154ed84a7b2f8a953f2f6d00403c5ea16081
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-17 14:32:40 +0500

    MFY auto commit at 17/12/2021 14:20:10

--- Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Login/Intro/Introduction.dart
+++ Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Login/Intro/Introduction.dart
@@ -8,70 +8,79 @@ import 'package:fb_login_app/Pages/Login/SignUp/SignUpSrceen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class IntroSrceen extends StatefulWidget {
-  const IntroSrceen({Key? key}) : super(key: key);
+class IntroSrceen extends StatelessWidget {
+  const IntroSrceen({ Key? key }) : super(key: key);
 
   @override
-  _IntroSrceenState createState() => _IntroSrceenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: IntroSrceenDetails(),
+      )
+    );
+  }
 }
 
-class _IntroSrceenState extends State<IntroSrceen> {
+class IntroSrceenDetails extends StatefulWidget {
+  const IntroSrceenDetails({Key? key}) : super(key: key);
+
+  @override
+  _IntroSrceenDetailsState createState() => _IntroSrceenDetailsState();
+}
+
+class _IntroSrceenDetailsState extends State<IntroSrceenDetails> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Welcome to "),
-                    Text(
-                      "Kolachi",
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                  ],
-                ),
-                Text(
-                  "Explore Us",
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
-              ],
-            ),
-            SizedBox(
-              child: Image.asset("assets/images/firstTime.png"),
-              width: getSize(true, 0.8),
-              height: getSize(false, 0.4),
-            ),
-            Column(
-              children: [
-                SizedBox(
-                  width: getSize(true, .7),
-                  child: ButtonColored(
-                      text: "Login",
-                      function: () {
-                        onClick(true);
-                      }),
-                ),
-                SizedBox(
-                  height: getSize(false, .01),
-                ),
-                ButtonText(
-                    text: "Signup",
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Welcome to "),
+                  Text(
+                    "Kolachi",
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                ],
+              ),
+              Text(
+                "Explore Us",
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ],
+          ),
+          SizedBox(
+            child: Image.asset("assets/images/firstTime.png"),
+            width: getSize(true, 0.8),
+            height: getSize(false, 0.4),
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: getSize(true, .7),
+                child: ButtonColored(
+                    text: "Login",
                     function: () {
-                      onClick(false);
-                    })
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+                      onClick(true);
+                    }),
+              ),
+              SizedBox(
+                height: getSize(false, .01),
+              ),
+              ButtonText(
+                  text: "Signup",
+                  function: () {
+                    onClick(false);
+                  })
+            ],
+          ),
+        ],
+      );
   }
 
   void onClick(bool direction) {
