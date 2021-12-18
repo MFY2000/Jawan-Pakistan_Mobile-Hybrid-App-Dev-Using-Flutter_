ff6624a4bf40410febcb6664a598579655f4a95a
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-18 12:26:14 +0500

    MFY auto commit at 18/12/2021 12:25:57

--- Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Login/SignUp/SignUpSrceen.dart
+++ Tasks/Task10 (Login app firebase)/fb_login_app/lib/Pages/Login/SignUp/SignUpSrceen.dart
@@ -4,6 +4,7 @@ import 'package:fb_login_app/Components/Custom/Alert/AlertBox1.dart';
 import 'package:fb_login_app/Components/Custom/Button/ButtonColored.dart';
 import 'package:fb_login_app/Components/Custom/TextFeild/PasswordFeild.dart';
 import 'package:fb_login_app/Components/Custom/TextFeild/TextFeild_1.dart';
+import 'package:fb_login_app/Config/Validition.dart';
 import 'package:fb_login_app/Config/constants.dart';
 import 'package:fb_login_app/Config/size_config.dart';
 import 'package:fb_login_app/Model/TextFeildModel.dart';
@@ -70,49 +71,36 @@ class _SignUpSrceenState extends State<SignUpSrceen> {
 
   onSigupClick() {
     bool isError = false;
+    var isName = validtionConstantName(controller[0].value);
+    var isEmail = validtionConstantEmail(controller[1].value);
+    var isPassword = validtionConstantPassword(controller[2].value);
 
-    if (!(controller[0].isFill)) {
+
+    if(isName[0]){
       setState(() {
-        controller[0].isError = true;
-        controller[0].errorMessage = kNamelNullError;
-        isError = true;
-      });
-    } else if (controller[0].value.length < 2) {
-      setState(() {
-        controller[0].isError = true;
-        controller[0].errorMessage = kShortNameError;
-        isError = true;
+        controller[0].isError = isName[0];
+        controller[0].errorMessage = isName[1];
+        isError =  isName[0];
       });
     }
 
-    if (!(controller[1].isFill)) {
+    if (isEmail[0]) {
       setState(() {
-        controller[1].isError = true;
-        controller[1].errorMessage = kEmailNullError;
-        isError = true;
-      });
-    } else if (!(controller[1].value.contains(emailValidatorRegExp))) {
-      setState(() {
-        controller[1].isError = true;
-        controller[1].errorMessage = kInvalidEmailError;
-        isError = true;
+        controller[1].isError = isEmail[0];
+        controller[1].errorMessage = isEmail[1];
+        isError =  isEmail[0];
       });
     }
 
-    if (!(controller[2].isFill)) {
+    if(isPassword[0]){
       setState(() {
-        controller[2].isError = true;
-        controller[2].errorMessage = kPassNullError;
-        isError = true;
-      });
-    } else if (controller[2].value.length < 6) {
-      setState(() {
-        controller[2].isError = true;
-        controller[2].errorMessage = kShortPassError;
-        isError = true;
+        controller[2].isError = isPassword[0];
+        controller[2].errorMessage = isPassword[1];
+        isError =  isPassword[0];
       });
     }
 
+   
     if (!isError) {
       onSigUp();
     }
